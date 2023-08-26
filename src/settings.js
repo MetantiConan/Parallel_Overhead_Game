bdf08e4371c192b119150572074279249fbe6183
MetantiConan <sonarfarouq@gmail.com>
2023-08-26 14:13:34 +0300

    setting for the whole things

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+
+Settings =
+{
+	game:
+	{
+		rings:200, sectors:10, ships:2, ship_depth:1, start_sector:2,
+		color_transitions: {min:12, max:60}
+	},
+	difficulty:
+	{
+		speed:0.000075, uncarved_safe_chance:0.5, carvers:2, transition_length:8,
+		increase: {speed:0, uncarved_safe_chance:-0.25, carvers:-1, transition_length:0}
+	},
+	hud: {sector:2, depth:3},
+	tick_time: {max: 24}
+}
